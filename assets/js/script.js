d9bcc5eb16b2606dcedd04a7a7fca47dc70c0203
Alex Wiles <alexandra.wiles6@gmail.com>
2022-03-02 23:18:53 -0500

    Enabled dragging and dropping tasks to delete them

--- assets/js/script.js
+++ assets/js/script.js
@@ -177,5 +177,62 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", event.target);
+  },
+  over: function(event) {
+    console.log("deactive", this);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    $(this).children().each(function() {
+      var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+
+        tasks[arrName] = tempArr;
+
+        saveTasks();
+
+      var text =$(this)
+        .find("p")
+        .text()
+        .trim();
+
+        var date = $(this)
+          .find("span")
+          .text()
+          .trim()
+
+      console.log(text,date);
+    });
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // load tasks for the first time
 loadTasks();
